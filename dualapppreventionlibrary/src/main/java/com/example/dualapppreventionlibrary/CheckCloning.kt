2a5d3ae2dcf8b412e8b84378e7e7422eb40fb001
OlaitanGbindin <olaitan@kudabank.com>
2022-02-23 15:28:16 +0100

    First commit

---
+++ dualapppreventionlibrary/src/main/java/com/example/dualapppreventionlibrary/CheckCloning.kt
@@ -0,0 +1,51 @@
+package com.example.dualapppreventionlibrary
+
+import android.app.Activity
+import android.content.Context
+import android.util.Log
+import kotlin.system.exitProcess
+
+object CheckCloning {
+
+    private const val packageDotsNumbers = 3
+
+    fun checkAppCloning(
+        context: Context,
+        onAppCloned: () -> Unit,   // what to do when the app is cloned
+        onAppNotCloned: () -> Unit // what to do when the app is genuine cloned
+    ) {
+        val path: String = context.filesDir?.path ?: ""
+        Log.d("path", "$path")
+        if (path.contains("999")) {
+            onAppCloned()
+        } else {
+            val count = getDotCount(path)
+            if (count > packageDotsNumbers) {
+                onAppCloned()
+            } else {
+                onAppNotCloned()
+            }
+        }
+    }
+
+    private fun getDotCount(path: String): Int {
+        var count = 0
+        for (element in path) {
+            if (count > packageDotsNumbers) {
+                break
+            }
+            if (element == '.') {
+                count++
+            }
+        }
+        return count
+    }
+
+    // This function closes the app
+    fun killProcess(activity: Activity) {
+        activity.finish()
+        android.os.Process.killProcess(android.os.Process.myPid())
+        exitProcess(0)
+    }
+
+}
